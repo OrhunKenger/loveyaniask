8b8e5cc6c31620da929096fc0a43b8f773951f74
Orhun Kenger <orhunkenger1929@gmail.com>
2026-05-31 00:13:18 +0300

    Initial Commit

---
+++ Loveyaniask/LoveyaniaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveyaniaskApp.swift
+//  Loveyaniask
+//
+//  Created by Şevval Ay Alış on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct LoveyaniaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
